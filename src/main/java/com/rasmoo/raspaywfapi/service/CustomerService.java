3244b8b80e2bdf01568fba9dde9e24d1394164e9
Felipe Alves <felipe.barbosa@ext.valid.com>
2023-12-16 01:43:43 -0300

    Add findAll para o customer

--- src/main/java/com/rasmoo/raspaywfapi/service/CustomerService.java
+++ src/main/java/com/rasmoo/raspaywfapi/service/CustomerService.java
@@ -2,9 +2,12 @@ package com.rasmoo.raspaywfapi.service;
 
 import com.rasmoo.raspaywfapi.dto.CustomerDto;
 import com.rasmoo.raspaywfapi.model.Customer;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface CustomerService {
 
     Mono<Customer> create(CustomerDto dto);
+
+    Flux<Customer> findAll(String firstName, String email, String cpf, int pageNumber, int pageSize, String sort);
 }
